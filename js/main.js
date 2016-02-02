740184113842668c6511b20d2472e844ea5a3339
Graphic <graphic@SOD-SJH-605-13M.local>
2016-02-02 16:33:34 -0500

    hello

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+$('button').on('click', function() {
+	
+	$('.on').toggleClass('off');
 
+	if ($('button').hasClass('off')) {
+		$('body').addClass('dark');
+		$('.status').text('Hey, who turned off the lights?')
+	}
+	else {
+		$('body').removeClass('dark');
+		$('.status').text('Its so bright in here!')
+	}
+	
+
+
+});
 
